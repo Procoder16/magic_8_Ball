2b6ab04665c455eaf7729f727191049dce963031
Procoder16 <smsoumik1608@gmail.com>
2021-03-28 00:38:36 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Ball(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Container(
+          child: Image.asset('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
